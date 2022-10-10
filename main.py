2cd4e70c6f05173e1b62be7ca5dd23bfb6374fd6
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-09 22:28:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 # ******************************
 # Make your Code
 # ******************************
+names = []
+
+for i in range(5):
+    names.append(input())
+
+shortest = names[0]
+longest = names[0]
+
+for name in names:
+    if len(name) > len(longest):
+        longest = name
+    elif len(name) < len(shortest):
+        shortest = name
+    elif len(name) == len(longest):
+        if ord(name[0]) < ord(longest[0]):
+            longest = name
+    elif len(name) == len(shortest):
+        if ord(name[0]) < ord(shortest[0]):
+            shortest = name
+
+print(shortest, longest)
